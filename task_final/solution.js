df0d828ac638e6724709848609993f48c56cfc88
MischukIA <playmischuk@gmail.com>
2022-01-04 17:43:27 +0300

    final1.2

--- task_final/solution.js
+++ task_final/solution.js
@@ -38,7 +38,7 @@ function sendRequest(name, phone, address, goods, sum) {
     let data = { client: '', order: {}, goods: [] };
 
     data.client = `${name} ${phone}`;
-    data.order.address = `$ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв. ${address.flat }`;
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat }`;
     data.order.sum = sum;
 
     for (let good of goods) {
